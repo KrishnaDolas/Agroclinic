2ce26feaf3f8a4187655ab991ccc38e86faffb57
Krishna Dolas <kdolas99@gmail.com>
2024-09-14 02:08:54 +0530

    wee

--- Components/Onbording/FirstScreenslider.tsx
+++ Components/Onbording/FirstScreenslider.tsx
@@ -7,19 +7,19 @@ const slides = [
     id: 1,
     title: 'Discover Best Places',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    image: require('./assets/onboardScreen1.png')
+    image: require('./src/assets/onboardScreen1.png')
   },
   {
     id: 2,
     title: 'Choose A Tasty Dish',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    image: require('./assets/onboardScreen2.png')
+    image: require('./src/assets/onboardScreen2.png')
   },
   {
     id: 3,
     title: 'Pick Up The Delivery',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    image: require('./assets/onboardScreen3.png')
+    image: require('./src/assets/onboardScreen3.png')
   }
 ]
 
